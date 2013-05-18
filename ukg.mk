8449609e4851d68a5b89e1cdefa9c8142a05d67d
RobMacGregor <robmac8138@gmail.com>
2013-05-18 19:58:14 -0300

    changed aosp folder stuff.

--- ukg.mk
+++ ukg.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/ukg/config/common.mk)
 
 # Grouper Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/grouper
+PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/grouper
 
 # Tablet Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/common_tablet
+PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/common_tablet
 
 # Setup device specific product configuration.
-PRODUCT_NAME := xylon_grouper
+PRODUCT_NAME := ukg_grouper
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
@@ -21,7 +21,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_ID=JDQ39 BUILD_FINGERP
 
 # Copy maguro specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/aosp/prebuilt/hybrid_tvdpi.conf:system/etc/beerbong/properties.conf \
-    vendor/aosp/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/ukg/prebuilt/hybrid_tvdpi.conf:system/etc/beerbong/properties.conf \
+    vendor/ukg/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/ukg/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
